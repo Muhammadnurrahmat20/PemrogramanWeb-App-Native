4d7242707872cf7e0d873064b16955043c6557bf
Muhammadnurrahmat20 <mnurrahmat20@mhs.akba.ac.id>
2023-01-17 21:05:29 +0800

    project final

---
+++ app-himti-native/home.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-6 text-center">
+      <p>&nbsp;</p>
+      <p>NIM : 20202205088</p>
+      <p>Nama Mahasiswa : Muhammad Nur Rahmat</p>
+    </div>
+  </div>
+</div>
